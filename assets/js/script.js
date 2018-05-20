ff873f7029dbabc980a355287ef8841ea28a3e52
Mike Corbett <srcorbetto@gmail.com>
2018-05-19 22:28:43 -0500

    new background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  console.log('connected');
+  // console.log('connected');
   var shadow = $('.nav-shadow').position();
 
   $(document).scroll(function(){
 
     if ( $(document).scrollTop() > 15 ) {
-      console.log('Not at top');
+      // console.log('Not at top');
       $('.nav-shadow').css('background', 'rgba(0, 0, 0, 0.25)');
     } else {
       $('.nav-shadow').css('background', 'transparent');
