e953143a1129d44c80bf93ad6e8b273a0f44e277
Mike Corbett <srcorbetto@gmail.com>
2018-05-27 16:45:56 -0500

    New layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,49 @@
 $(document).ready(function(){
-  // console.log('connected');
+  
+  var expanded = false;
   var shadow = $('.nav-shadow').position();
 
-  $(document).scroll(function(){
+  // Shadow scroll
+  if (!expanded) {
+    $(document).scroll(function(){
 
-    if ( $(document).scrollTop() > 15 ) {
-      // console.log('Not at top');
-      $('.nav-shadow').css('background', 'rgba(0, 0, 0, 0.25)');
+      if ( $(document).scrollTop() > 15 ) {
+        // console.log('Not at top');
+        $('.nav-shadow').css('background', 'rgba(0, 0, 0, 0.25)');
+      } else {
+        $('.nav-shadow').css('background', 'transparent');
+      }
+  
+    });
+  }
+
+  $('.mobile-btn').click(function(){
+    $('.menu-bar').css({
+      'transition': '.5s'
+    });
+    if ($('.mobile-btn p').text() === 'MENU') {
+      $('.menu-bar').css({
+        'left': '0px'
+      });
+      $('.mobile-btn p').text('CLOSE');
     } else {
-      $('.nav-shadow').css('background', 'transparent');
+      $('.menu-bar').css('left', '-250px');
+      $('.mobile-btn p').text('MENU');
     }
-
   });
+
+  // Window Resize Listener...
+  $(window).resize(function() {
+    if ($(window).width() > 1200) {
+      $('.menu-bar').css({
+        'left': '0',
+        'transition': 'none'
+      });
+    } else {
+      $('.menu-bar').css({
+        'left': '-250px'
+      });
+    }
+  });
+
 });
